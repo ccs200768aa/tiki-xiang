661a5b5b9f21073eb1007e5a549427a82cd56c77
ccs200768aa <100597218@qq.com>
2017-10-09 15:46:30 +0800

    博客

--- js/blog.js
+++ js/blog.js
@@ -1,7 +1,13 @@
 /**
  * Created by tiki on 2017/6/21.
  */
-var url_prefix = "http://localhost:8990/pipes/";
+
+$(function () {
+    loadBlogList();
+    loadBlogCategoryList();
+});
+
+var url_prefix = "http://localhost:8990/indexManage/pipes/";
 
 function loadBlogList(categoryid) {
     var blogListUrl = url_prefix + "blog/blogArticalList"
@@ -16,7 +22,7 @@ function loadBlogList(categoryid) {
                 + "<span>时间</span><span>类型</span><span>分享</span>"
                 + "<p>" + content
                 + "</p>"
-                + "<button class='btn btn-blog-info'><a href='javascript:void(0);' onclick='getArticalDetail(" + articalId + ");'>了解更多</></button>"
+                + "<button class='btn btn-blog-info'><a style='color: #FFFFFF' href='javascript:void(0);' onclick='getArticalDetail(" + articalId + ");'>了解更多</></button>"
                 + "<div class='clearfix'></div>"
                 + "<hr/>"
                 + "</div>";
@@ -39,19 +45,19 @@ function getArticalDetail(articalId) {
     window.location.href = detailHtml;
 }
 
-function loadBlogCategoryList(){
+function loadBlogCategoryList() {
     var blogListUrl = url_prefix + "blog/blogCategoryList";
     $.get(blogListUrl, function (res) {
         $.each(res, function (index, val) {
             var categoryId = res[index].categoryId;
             var title = res[index].name;
-            var html = "<div class='dropdown'> <button type='button' class='btn dropdown-toggle' id='dropdownMenu"+(index +1)+"' data-toggle='dropdown'>" + title
-            +"<span class='caret'></span>"
-            +" </button>"
-            +"  <ul class='dropdown-menu' role='menu' aria-labelledby='dropdownMenu"+(index +1)+"'>"
-            +"  <li role='presentation'>"
-            +"  <a role='menuitem' tabindex='-1' href='#'>" + title+ "</a>"
-            +"  </li></ul></div>";
+            var html = "<div class='dropdown'> <button type='button' class='btn dropdown-toggle' id='dropdownMenu" + (index + 1) + "' data-toggle='dropdown'>" + title
+                + "<span class='caret'></span>"
+                + " </button>"
+                + "  <ul class='dropdown-menu' role='menu' aria-labelledby='dropdownMenu" + (index + 1) + "'>"
+                + "  <li role='presentation'>"
+                + "  <a role='menuitem' tabindex='-1' href='#'>" + title + "</a>"
+                + "  </li></ul></div>";
 
             $(".blog-category").append(html);
         });
